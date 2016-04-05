df3bda3c95c55476e44066416a51d060018bd833
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2016-04-05 17:02:51 +0100

    Add my zshrc, add install for oh-my-zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
-alias reload!='. ~/.zshrc'
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias reload!=". ~/.zshrc"
+alias trash="rmtrash"
