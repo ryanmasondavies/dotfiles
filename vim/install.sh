df61b7a36dc5dbbb7f0d1cddc38e2458a8bad0c1
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2016-05-02 00:34:40 +0100

    Install Pathogen and handy Vim packages

---
+++ vim/install.sh
@@ -0,0 +1,30 @@
+#
+# Install Pathogen for Vim packages
+# Source: https://github.com/tpope/vim-pathogen
+#
+
+if [[ ! -e ~/.vim/autoload ]]; then
+	echo "Installing Pathogen..."
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+if [[ ! -e ~/.vim/bundle/vim-sensible ]]; then
+	echo "Installing vim-sensible..."
+	git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
+fi
+
+if [[ ! -e ~/.vim/bundle/vim-colors-solarized ]]; then
+	echo "Installing vim-colors-solarized..."
+	git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+fi
+
+if [[ ! -e ~/.vim/bundle/vim-airline ]]; then
+	echo "Installing vim-airline..."
+	git clone git://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
+fi
+
+if [[ ! -e ~/.vim/bundle/vim-airline-themes ]]; then
+	echo "Installing vim-airline-themes..."
+	git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+fi
