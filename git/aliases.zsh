3c20af3d7cc2c93992c6f8b30c626cb133be3338
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2016-10-25 14:16:27 +0100

    Add git alias for force push branch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,4 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gpp='git push origin +$(git rev-parse --abbrev-ref HEAD)'
