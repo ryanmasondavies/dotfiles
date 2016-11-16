1b37259aed24902ced1a12e3579e13588492a92f
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2016-11-16 10:53:07 +0000

    Add new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,15 @@
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-#
-# Example aliases
+
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
 alias reload!=". ~/.zshrc"
 alias trash="rmtrash"
+
+alias v='f -e vim' # quick opening files with vim
+alias t='f -a -e sublime'
+alias m='f -e mplayer' # quick opening files with mplayer
+alias o='a -e open' # quick opening files with xdg-open
+
+alias sb='sublime'
