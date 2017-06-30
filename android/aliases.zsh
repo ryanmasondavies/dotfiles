ffa008b90a74df416425b9832710d58b88b4f311
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2017-06-30 12:14:03 +0100

    Add android_emulate_tablet alias

--- android/aliases.zsh
+++ android/aliases.zsh
@@ -1,2 +1,3 @@
 alias android_emulate='cd $(dirname $(which emulator)) && ./emulator -avd Nexus_5_API_23'
+alias android_emulate_tablet='cd $(dirname $(which emulator)) && ./emulator -avd Pixel_C_API_23'
 alias react_native_android_logs='adb logcat | grep ReactNativeJS'
