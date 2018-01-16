0260656bfaa6af362508199cb826e7667affd9e8
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2018-01-16 15:46:23 +0000

    Remove PaperColor vim theme

--- vim/install.sh
+++ vim/install.sh
@@ -29,8 +29,3 @@ if [[ ! -e ~/.vim/bundle/nerdtree ]]; then
     git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 fi
 
-if [[ ! -e ~/.vim/bundle/papercolor-theme ]]; then
-    echo "Installing papercolor-theme..."
-    git clone https://github.com/NLKNguyen/papercolor-theme.git ~/.vim/bundle/papercolor-theme
-fi
-
