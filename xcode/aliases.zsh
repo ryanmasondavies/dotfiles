4ce2ff886fd4d99d9ea3e16c63e30177d7e6e058
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2019-09-06 14:46:52 +0100

    Add x alias for opening Xcode projects

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,3 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias x='matches=("xcworkspace" "xcodeproj" "playground"); for i in "${matches[@]}"; do if [ -d *.${i} ]; then open -a Xcode *.${i}; break; fi; done'
+
