70d21504a8d1bec8c61244bd11820e94c717d326
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2021-01-22 10:02:49 +0000

    Use Plug for Vim dependencies

--- vim/install.sh
+++ vim/install.sh
@@ -3,29 +3,7 @@
 # Source: https://github.com/tpope/vim-pathogen
 #
 
-if [[ ! -e ~/.vim/autoload ]]; then
-	echo "Installing Pathogen..."
-	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-fi
 
-if [[ ! -e ~/.vim/bundle/vim-sensible ]]; then
-	echo "Installing vim-sensible..."
-	git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
-fi
-
-if [[ ! -e ~/.vim/bundle/vim-airline ]]; then
-	echo "Installing vim-airline..."
-	git clone git://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
-fi
-
-if [[ ! -e ~/.vim/bundle/vim-airline-themes ]]; then
-	echo "Installing vim-airline-themes..."
-	git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-fi
-
-if [[ ! -e ~/.vim/bundle/nerdtree ]]; then
-    echo "Installing NERDTree..."
-    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-fi
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
