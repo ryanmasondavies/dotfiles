70acc90a71bbaafb9ddb7574c0e530e1c5bb9a07
Ryan Mason-Davies <625199+ryanmasondavies@users.noreply.github.com>
2021-07-23 10:04:11 +0100

    Set dock to bottom

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -69,8 +69,8 @@ defaults write com.apple.dock mru-spaces -bool false
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
-# Set dock to the left side
-defaults write com.apple.dock orientation -string left
+# Set dock to bottom
+defaults write com.apple.dock orientation -string bottom
 
 # Hot corners
 # Disable all hot corners
